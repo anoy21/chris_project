28483e07c8d1ca6e0bbaa3dd1b1f2f07b6a332cd
anoy21 <thienhung.kp2@gmail.com>
2023-12-13 19:21:08 +0700

    newest

--- main/app.js
+++ main/app.js
@@ -9,13 +9,75 @@ window.addEventListener('scroll', scrollHeader)
 
 // locomotive
 
-const scroller = new LocomotiveScroll({
-    el: document.querySelector("[data-scroll-container]"),
-    smooth: true,
-    tablet: { smooth: true },
-    smartphone: { smooth: true },
-    multiplier: 0.5,
-  });
+const body = document.body;
+const backToTop = document.querySelector(".back-to-top");
+const opacityClass = "opacity-0";
+const visibilityClass = "invisible";
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+  tablet: { smooth: true },
+  smartphone: { smooth: true },
+  multiplier: 0.5,
+}); 
+
+const arrayOfColors = [
+  "#0a9396",
+  "#005f73",
+  "#ae2012",
+  "#3d405b",
+  "#003049",
+  "#bc6c25",
+  "#ff006e",
+  "#ef476f",
+  "#1982c4",
+  "#ee964b",
+  "#0ead69",
+  "#390099",
+  "#f6aa1c",
+  "#54101d",
+  "#2b2c28",
+  "#85c7f2",
+  "#e15a97",
+  "#2b70e3",
+  "#b36a5e"
+];
+
+function getRandomColor() {
+  const arrayLength = arrayOfColors.length;
+  const randomValue = Math.random() * arrayLength;
+  const roundedNumber = Math.floor(randomValue);
+  const color = arrayOfColors[roundedNumber];
+  return color;
+}
+
+scroll.on("call", (value, way, obj) => {
+  if (value === "randomizeTextColor") {
+    if (way === "enter") {
+      obj.el.style.color = getRandomColor();
+    }
+  } else if (value === "toggleBackToTop") {
+    if (way === "enter") {
+      backToTop.classList.add(opacityClass, visibilityClass);
+    } else {
+      backToTop.classList.remove(opacityClass, visibilityClass);
+    }
+  }
+});
+
+scroll.on("scroll", (instance) => {
+  const visibleSubSectionHeading = document.querySelector(
+    ".sub-section h2.is-inview"
+  );
+
+  if (visibleSubSectionHeading) {
+    const parentSection = visibleSubSectionHeading.parentElement.parentElement;
+    body.style.backgroundColor = parentSection.dataset.bgColor;
+  } else {
+    body.style.backgroundColor = "";
+  }
+});
 
 
 
@@ -25,119 +87,4 @@ const scroller = new LocomotiveScroll({
 
   
 
-//snow
 
-(function() {
-  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
-  function(callback) {
-      window.setTimeout(callback, 1000 / 60);
-  };
-  window.requestAnimationFrame = requestAnimationFrame;
-})();
-
-
-var flakes = [],
-  canvas = document.getElementById("canvas"),
-  ctx = canvas.getContext("2d"),
-  flakeCount = 300,
-  mX = -100,
-  mY = -100
-
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-function snow() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  for (var i = 0; i < flakeCount; i++) {
-      var flake = flakes[i],
-          x = mX,
-          y = mY,
-          minDist = 150,
-          x2 = flake.x,
-          y2 = flake.y;
-
-      var dist = Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y)),
-          dx = x2 - x,
-          dy = y2 - y;
-
-      if (dist < minDist) {
-          var force = minDist / (dist * dist),
-              xcomp = (x - x2) / dist,
-              ycomp = (y - y2) / dist,
-              deltaV = force / 2;
-
-          flake.velX -= deltaV * xcomp;
-          flake.velY -= deltaV * ycomp;
-
-      } else {
-          flake.velX *= .98;
-          if (flake.velY <= flake.speed) {
-              flake.velY = flake.speed
-          }
-          flake.velX += Math.cos(flake.step += .05) * flake.stepSize;
-      }
-
-      ctx.fillStyle = "rgba(255,255,255," + flake.opacity + ")";
-      flake.y += flake.velY;
-      flake.x += flake.velX;
-          
-      if (flake.y >= canvas.height || flake.y <= 0) {
-          reset(flake);
-      }
-
-
-      if (flake.x >= canvas.width || flake.x <= 0) {
-          reset(flake);
-      }
-
-      ctx.beginPath();
-      ctx.arc(flake.x, flake.y, flake.size, 0, Math.PI * 2);
-      ctx.fill();
-  }
-  requestAnimationFrame(snow);
-};
-
-function reset(flake) {
-  flake.x = Math.floor(Math.random() * canvas.width);
-  flake.y = 0;
-  flake.size = (Math.random() * 3) + 2;
-  flake.speed = (Math.random() * 1) + 0.5;
-  flake.velY = flake.speed;
-  flake.velX = 0;
-  flake.opacity = (Math.random() * 0.5) + 0.3;
-}
-
-function init() {
-  for (var i = 0; i < flakeCount; i++) {
-      var x = Math.floor(Math.random() * canvas.width),
-          y = Math.floor(Math.random() * canvas.height),
-          size = (Math.random() * 3) + 2,
-          speed = (Math.random() * 1) + 0.5,
-          opacity = (Math.random() * 0.5) + 0.3;
-
-      flakes.push({
-          speed: speed,
-          velY: speed,
-          velX: 0,
-          x: x,
-          y: y,
-          size: size,
-          stepSize: (Math.random()) / 30,
-          step: 0,
-          opacity: opacity
-      });
-  }
-
-  snow();
-};
-
-canvas.addEventListener("mousemove", function(e) {
-  mX = e.clientX,
-  mY = e.clientY
-});
-
-window.addEventListener("resize",function(){
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-})
